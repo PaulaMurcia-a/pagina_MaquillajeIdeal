805cbd8a8a51bf15bd6203a1afb8e9c685767108
PaulaMurcia-a <pmmurcia78@ucatolica.edu.co>
2026-05-25 17:31:55 -0500

    [ADD] MODELS

---
+++ models.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class Producto(BaseModel):
+    nombre: str
+    marca: str
+    tipo_piel_id: int
+    categoria_id: int
+
+
+class Categoria(BaseModel):
+    nombre: str
+
+
+class TipoPiel(BaseModel):
+    nombre: str
+    descripcion: str
